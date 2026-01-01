c62b1acdcf1d556d79625428fb1f9428148ddfaf
TylerSpaeth <tspaeth58@gmail.com>
2026-01-01 10:46:52 -0600

    Remove JavaFX code

--- src/main/java/com/github/tylerspaeth/Main.java
+++ src/main/java/com/github/tylerspaeth/Main.java
@@ -3,18 +3,14 @@ package com.github.tylerspaeth;
 import com.github.tylerspaeth.common.data.dao.StrategyDAO;
 import com.github.tylerspaeth.config.DatasourceConfig;
 import com.github.tylerspaeth.strategy.StrategyRegistry;
-import com.github.tylerspaeth.ui.GUI;
-import javafx.application.Application;
 
 public class Main {
     public static void main(String[] args) {
 
-        StrategyRegistry strategyRegistry = new StrategyRegistry(new StrategyDAO());
-        strategyRegistry.initialize();
-
         DatasourceConfig.validate();
 
-        Application.launch(GUI.class, "");
+        StrategyRegistry strategyRegistry = new StrategyRegistry(new StrategyDAO());
+        strategyRegistry.initialize();
 
     }
 }
