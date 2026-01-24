7826dae8a7076ce04b1ee04b8ef4e31695f49786
TylerSpaeth <tspaeth58@gmail.com>
2026-01-24 09:54:24 -0600

    Build out abstract strategy functionality

--- src/main/java/com/github/tylerspaeth/strategy/AbstractStrategy.java
+++ src/main/java/com/github/tylerspaeth/strategy/AbstractStrategy.java
@@ -1,8 +1,19 @@
 package com.github.tylerspaeth.strategy;
 
-import com.github.tylerspaeth.common.data.entity.Strategy;
+import com.github.tylerspaeth.common.data.dao.OrderDAO;
+import com.github.tylerspaeth.common.data.entity.BacktestResult;
+import com.github.tylerspaeth.common.data.entity.Order;
+import com.github.tylerspaeth.common.data.entity.StrategyParameterSet;
+import com.github.tylerspaeth.engine.EngineCoordinator;
+import com.github.tylerspaeth.engine.request.AbstractEngineRequest;
+import com.github.tylerspaeth.strategy.annotation.StrategyParameterLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.sql.Timestamp;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * Class that all strategies must inherit from.
@@ -11,18 +22,130 @@ public abstract class AbstractStrategy {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AbstractStrategy.class);
 
-    private Strategy strategyEntity;
+    private final OrderDAO orderDAO;
 
-    public void setStrategyEntity(Strategy strategyEntity) {
-        if(this.strategyEntity != null) {
-            this.strategyEntity = strategyEntity;
-        } else {
-            LOGGER.error("Strategy entity can not be updated.");
-        }
+    private EngineCoordinator engineCoordinator;
+
+    private final StrategyParameterSet strategyParameterSet;
+    private final AtomicReference<BacktestResult> backtestResult;
+
+    // Currently this can only be set true once. If a strategy should be run again then a new instance should be created.
+    private final AtomicBoolean running = new AtomicBoolean(false);
+    private volatile Thread runningThread;
+
+    public AbstractStrategy(StrategyParameterSet strategyParameterSet) {
+        this.strategyParameterSet = strategyParameterSet;
+        this.backtestResult = null;
+        this.orderDAO = new OrderDAO();
     }
 
-    public Strategy getStrategyEntity() {
-        return strategyEntity;
+    public AbstractStrategy(StrategyParameterSet strategyParameterSet, BacktestResult backtestResult) {
+        this.strategyParameterSet = strategyParameterSet;
+        this.backtestResult = new AtomicReference<>(backtestResult);
+        this.orderDAO = new OrderDAO();
+    }
+
+    /**
+     * Runs the strategy.
+     */
+    public final void run() {
+
+        if(engineCoordinator == null) {
+            LOGGER.error("Unable to run a strategy without an EngineCoordinator.");
+            return;
+        }
+
+        if(!running.compareAndSet(false, true)) {
+            LOGGER.error("Unable to run a strategy that is already running.");
+            return;
+        }
+
+        StrategyParameterLoader.populateParameters(this, strategyParameterSet);
+
+        Thread thread = new Thread(() -> {
+            try {
+                if (backtestResult != null) {
+                    backtestResult.updateAndGet(result -> {
+                        result.setStartTime(new Timestamp(System.currentTimeMillis()));
+                        return result;
+                    });
+                }
+
+                onRun();
+            } finally {
+                if(backtestResult != null) {
+                    backtestResult.updateAndGet(result -> {
+                        result.setEndTime(new Timestamp(System.currentTimeMillis()));
+                        return result;
+                    });
+                }
+            }
+        }, strategyParameterSet.toString() + "-Thread");
+        thread.start();
+        runningThread = thread;
+
+        LOGGER.info("{} started running with {} parameter set.", strategyParameterSet.getStrategy(), strategyParameterSet);
+    }
+
+    /**
+     * Defines the run lifecycle of a concrete strategy implementation. Somewhere in this method it should check if
+     * onStop has already been called and if so exit gracefully.
+     */
+    protected abstract void onRun();
+
+    /**
+     * Stops the strategy from continuing to run.
+     */
+    public final void stop() {
+
+        if(!running.get())   {
+            LOGGER.warn("Strategy already stopped. {}", strategyParameterSet.getStrategy());
+            return;
+        }
+
+        LOGGER.info("Stop has been triggered for strategy: {} and parameter set: {}", strategyParameterSet.getStrategy(), strategyParameterSet);
+        onStop();
+        LOGGER.info("Stop has completed for strategy: {} and parameter set: {}", strategyParameterSet.getStrategy(), strategyParameterSet);
+    }
+
+    /**
+     * The stop behavior that should be implemented by the concrete strategy class. After this is called, the onRun method
+     * should return if it has not already.
+     */
+    protected abstract void onStop();
+
+    /**
+     * Submits an engine request and blocks until it returns. This should only be called for a concrete strategy class
+     * to avoid blocking other threads.
+     * @param engineRequest AbstractEngineRequest
+     * @return Result of the engine request.
+     * @param <T> Type to be returned by the engine.
+     * @throws ExecutionException ExecutionException
+     * @throws InterruptedException InterruptedException
+     * @throws IllegalStateException If the EngineCoordinator is null.
+     */
+    protected final <T> T submitEngineRequest(AbstractEngineRequest<T> engineRequest) throws ExecutionException, InterruptedException, IllegalStateException {
+        if(engineCoordinator == null) {
+            throw new IllegalStateException("Failed to submit request to engine and the EngineCoordinator is null.");
+        }
+        T result = engineCoordinator.submitRequest(engineRequest);
+        if(result instanceof Order && backtestResult != null) {
+            ((Order)result).setBacktestResult(backtestResult.get());
+            orderDAO.update(((Order)result));
+        }
+        return result;
+    }
+
+    /**
+     * Set the EngineCoordinate for engine requests to be fed to. This can only be called once and must not be null.
+     * @param engineCoordinator EngineCoordinator that request will be submitted to.
+     */
+    public final void setEngineCoordinator(EngineCoordinator engineCoordinator) {
+        if(engineCoordinator != null && this.engineCoordinator == null) {
+            this.engineCoordinator = engineCoordinator;
+        } else {
+            LOGGER.error("Failed to set EngineCoordinator");
+        }
     }
 
 }
